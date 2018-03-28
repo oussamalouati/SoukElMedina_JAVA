8b15d72a014673bf2c3592fb26d9207946105295
oussamalouati <louati.oussama01@gmail.com>
2018-03-28 19:13:06 +0000

    Revert "Correction de l'affichage initial de l'interface du vendeur"
    This reverts commit 8f9f72fa6f303c477188cfc22c30284402c4de25.

--- src/tests/SoukElMedina.java
+++ src/tests/SoukElMedina.java
@@ -25,44 +25,45 @@ import javafx.stage.StageStyle;
  * @author INETEL
  */
 public class SoukElMedina extends Application {
-
-    /* Delta est  une classe qui contient deux variables quantitatives X et Y 
+       /* Delta est  une classe qui contient deux variables quantitatives X et Y 
     pour determiner la postion de la fenetre */
-
-    final Delta dragDelta = new Delta();
-
+        
+       final Delta dragDelta = new Delta(); 
     @Override
-
+    
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/gui/Acceuil.fxml"));
-
+        
         /* la ligne 33 permet de cacher le "windows bar/borders"  */
         stage.initStyle(StageStyle.TRANSPARENT);
-
-        /*les deux fonction "setOnMousePressed" et "setOnMouseDragged"
+            
+            /*les deux fonction "setOnMousePressed" et "setOnMouseDragged"
               servent à deplacer la fenetre.  */
-        root.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                dragDelta.x = stage.getX() - mouseEvent.getScreenX();
-                dragDelta.y = stage.getY() - mouseEvent.getScreenY();
-            }
-        });
-        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
+            root.setOnMousePressed(new EventHandler<MouseEvent>() {
+                @Override public void handle(MouseEvent mouseEvent) {
+                  dragDelta.x = stage.getX() - mouseEvent.getScreenX();
+                  dragDelta.y = stage.getY() - mouseEvent.getScreenY();
+                }
+              });
+            root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+              @Override public void handle(MouseEvent mouseEvent) {
                 stage.setX(mouseEvent.getScreenX() + dragDelta.x);
                 stage.setY(mouseEvent.getScreenY() + dragDelta.y);
-            }
-        });
-
+              }
+            });
+        
+            
+           
+            
         Scene scene = new Scene(root);
-
+        
+         
+         
         scene.setFill(Color.TRANSPARENT);// cette ligne sert a rendre l'arriere plan transparent .
-
+        
         stage.setScene(scene);
         stage.show();
-
+        
     }
 
     /**
@@ -71,5 +72,5 @@ public class SoukElMedina extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
+    
 }
